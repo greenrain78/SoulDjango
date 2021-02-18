f00f832dbed354da6b08500c1052449f5e7475e4
daewon kim <kimdw010130@daum.net>
2021-02-18 15:13:33 +0900

    make base.html include nav bar, css, js file

--- chicken_soup/models.py
+++ chicken_soup/models.py
@@ -1,18 +1,30 @@
 from django.db import models
 
-
 # Create your models here.
 from django.utils import timezone
 
 
 class User(models.Model):
+    # name
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
     nickname = models.CharField(max_length=30)
+
+    # number
     number = models.PositiveBigIntegerField(default=0)
     email = models.CharField(max_length=200)
+    correct = models.IntegerField(default=0)
+    submission = models.IntegerField(default=0)
+    answer_ratio = models.FloatField(default=0.5)
+
+    # time
     joined_time = models.DateTimeField(default=timezone.now)
     birthday = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.nickname
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
